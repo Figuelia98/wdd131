767287f664e6e0ab5488c0629c8eca95d3f0c28c
Figuelia98 <figueliayasin98@gmail.com>
2024-11-24 09:36:55 +0300

    Week 3 commit 2

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,3 +3,25 @@ const currentYear = document.querySelector("#currentyear")
 const today = new Date()
 currentYear.innerText = today.getFullYear()
 lastModDate.innerText = "Last modification :"+ document.lastModified
+const menu = document.querySelector("#menuList")
+const butMenu = document.querySelector(".menu")
+var x = window.matchMedia("(max-width:800px)") 
+x.onchange = (e)=>{
+    if(e.matches){
+        menu.style.display ="none"; 
+    }
+    else{
+        menu.style.display ="flex"; 
+    }
+}
+
+butMenu.addEventListener("click",function(){
+  
+    if(menu.style.display == "none"){
+        menu.style.display ="grid"
+    }
+    else{
+        menu.style.display = "none"
+    }
+
+})
