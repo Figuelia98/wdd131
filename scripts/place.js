dfc37b181a6517bff2a9cd752679f7c9ec8347a8
Figuelia98 <figueliayasin98@gmail.com>
2024-11-25 02:53:24 +0300

    Week 3 commit 7

--- scripts/place.js
+++ scripts/place.js
@@ -1,5 +1,9 @@
 const lastModDate = document.querySelector("#lastModified")
 const currentYear = document.querySelector("#currentyear") 
+const windChillcon = document.querySelector("#windchill")
 const today = new Date()
 currentYear.innerText = today.getFullYear()
 lastModDate.innerText = "Last modification :"+ document.lastModified
+let windChill = 35.74 + 0.6215*(86) - 35.75*(Math.pow(5,0.16)) + 0.4275*(86)*(Math.pow(5,0.16))
+windChill  = (windChill - 32) * 5/9
+windChillcon.innerHTML = "<li id='windchill'><span>Wind Chill:</span> "+parseInt(windChill)+"°C</li>"
